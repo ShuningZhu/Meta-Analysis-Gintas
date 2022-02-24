3af00d524fdad154401ca3116facba894dc3323a
Mac <mac@dhcp-wifi-8021x-168-122-207-160.bu.edu>
2022-02-24 17:11:22 -0500

    forest plot

--- Tree plot.R
+++ Tree plot.R
@@ -1,1 +1,26 @@
 #Hi, WELCOME!
+library(readxl)
+library(meta)
+LCQ <- read_excel("LCQ data.xlsx")
+head(LCQ)
+colnames(LCQ)
+names(LCQ)<-c('author','year','Ne','Nc','Me','Se','Mc','Sc')
+### Do meta analysis
+m <- metacont(Ne,Me,Se,Nc,Mc,Sc,
+              studlab=paste(author,year), # 
+              data=LCQ)
+
+forest(m, xlab="Maximum % fall in FEV1")
+
+
+VAS <- read_excel("VAS data.xlsx")
+head(VAS)
+colnames(VAS)
+names(VAS)<-c('author','year','Ne','Nc','Me','Se','Mc','Sc')
+### Do meta analysis
+m <- metacont(Ne,Me,Se,Nc,Mc,Sc,
+              studlab=paste(author,year), # 
+              data=VAS)
+
+forest(m, xlab="Maximum % fall in FEV1")
+
